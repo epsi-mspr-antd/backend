66765381d3fdd0b64a9777ffdccfbec7594965f8
Arnaud Gaydamour <arnaud.gaydamour@protonmail.com>
2024-05-23 13:00:37 +0200

    feat: update tests & add e2e tests

--- src/plant/plant.controller.spec.ts
+++ src/plant/plant.controller.spec.ts
@@ -1,61 +1,100 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PlantController } from './plant.controller';
 import { PlantService } from './plant.service';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { AddressService } from 'src/address/address.service';
+import { CreatePlantDto, UpdatePlantDto } from './dto';
 
 describe('PlantController', () => {
   let controller: PlantController;
-  let service: PlantService;
+
+  const plant = {
+    id: 1,
+    name: 'Test Plant',
+    species: { id: 1, name: 'test' },
+    status: { id: 1, name: 'test' },
+  };
+
+  const mockPlantService = {
+    findAll: jest.fn(),
+    findUserPlants: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PlantController],
-      providers: [PlantService, PrismaService, AddressService],
+      providers: [
+        {
+          provide: PlantService,
+          useValue: mockPlantService,
+        },
+      ],
     }).compile();
 
     controller = module.get<PlantController>(PlantController);
-    service = module.get<PlantService>(PlantService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
 
-  it('should find user plants', async () => {
-    const result = {
-      data: [
-        {
-          id: 1,
-          name: 'Plant 1',
-          species: {
-            id: 1,
-            name: 'Species 1',
-          },
-          status: {
-            id: 1,
-            name: 'Status 1',
-          },
-        },
-        {
-          id: 2,
-          name: 'Plant 2',
-          species: {
-            id: 1,
-            name: 'Species 1',
-          },
-          status: {
-            id: 1,
-            name: 'Status 1',
-          },
-        },
-      ],
-    };
+  describe('findAll', () => {
+    it('should return an array of plants', async () => {
+      jest.spyOn(controller, 'findAll').mockResolvedValue({ data: [plant] });
 
-    jest
-      .spyOn(service, 'findUserPlants')
-      .mockImplementation(() => Promise.resolve(result));
+      expect(controller.findAll()).resolves.toEqual({ data: [plant] });
+    });
+  });
 
-    expect(await controller.findUserPlants(1)).toBe(result);
+  describe('findUserPlants', () => {
+    it('should return an array of user plants', async () => {
+      jest
+        .spyOn(controller, 'findUserPlants')
+        .mockResolvedValue({ data: [plant] });
+
+      expect(controller.findUserPlants(1)).resolves.toEqual({ data: [plant] });
+    });
+  });
+
+  describe('create', () => {
+    it('should create and return a plant', async () => {
+      const createPlantDto: CreatePlantDto = {
+        name: 'Test Plant',
+        speciesId: 1,
+        statusId: 1,
+        addressId: 1,
+      };
+
+      jest.spyOn(controller, 'create').mockResolvedValue({ data: plant });
+
+      expect(controller.create(1, createPlantDto)).resolves.toEqual({
+        data: plant,
+      });
+    });
+  });
+
+  describe('update', () => {
+    it('should update and return a plant', async () => {
+      const updatePlantDto: UpdatePlantDto = {
+        name: 'Updated Plant',
+        speciesId: 1,
+        statusId: 1,
+      };
+
+      jest.spyOn(controller, 'update').mockResolvedValue({ data: plant });
+
+      expect(controller.update(1, 1, updatePlantDto)).resolves.toEqual({
+        data: plant,
+      });
+    });
+  });
+
+  describe('delete', () => {
+    it('should delete a plant', async () => {
+      jest.spyOn(controller, 'delete').mockResolvedValue(undefined);
+
+      expect(controller.delete(1, 1)).resolves.toBeUndefined();
+    });
   });
 });
